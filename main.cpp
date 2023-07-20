c4c4a4a4d01fbb155f3c4926002df56f838844a7
phsyobs <phsyobs@gmail.com>
2023-07-20 10:57:26 +0900

    메인 파일 추가

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define repeat(i, s, e) for(int i = s; i < e; i++)
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define compress(vec) sort(all(vec)); vec.erase(unique(all(vec)), vec.end());
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<double, double>;
+using int128 = __int128_t;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    
+    
+    return 0;
+}
